47099fa4ae131f694c9aa246e67977fa8f2ad9df
Aurelien Thierry <aurelien.thierry@inria.fr>
2014-10-07 19:17:07 +0200

    vrac

--- supports/disas_sm/ex2.asm
+++ supports/disas_sm/ex2.asm
@@ -3,6 +3,7 @@ global _start
 _start:
 
 debut:
+xor edi, edi
 inc eax
 jz zero ; saute si la nouvelle valeur d'eax est 0
 
@@ -13,8 +14,9 @@ jmp debut
 zero:
 mov edi, 1
 mov ebx, debut
-mov word [ebx], 0x90
-mov word [ebx+1], 0xc3
+mov word [ebx], 0x47
+mov word [ebx+1], 0x47
+mov word [ebx+3], 0xc3
 jmp debut
 
 
