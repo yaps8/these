5910d75725bb30516a9c09ccb501b3310c0f9bb7
Aurelien Thierry <aurelien.thierry@gmail.com>
2014-10-08 15:48:39 +0200

    assembleur

--- supports/disas_sm/ex2.asm
+++ supports/disas_sm/ex2.asm
@@ -3,7 +3,6 @@ global _start
 _start:
 
 debut:
-xor edi, edi
 inc eax
 jz zero ; saute si la nouvelle valeur d'eax est 0
 
@@ -14,9 +13,8 @@ jmp debut
 zero:
 mov edi, 1
 mov ebx, debut
-mov word [ebx], 0x47
-mov word [ebx+1], 0x47
-mov word [ebx+3], 0xc3
+mov word [ebx], 0x90
+mov word [ebx+1], 0xc3
 jmp debut
 
 
